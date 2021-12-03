a37f7e18e659bbd097ff84f94ef94503229b0485
Romimi <romina.palma@usach.cl>
2021-12-03 16:22:13 -0300

    ADD - Migraciones + archivos de modelos

---
+++ database/migrations/2021_12_03_191838_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name',100)->unique();
+            $table->text('image');
+            $table->text('description');
+            
+            $table->unsignedBigInteger('id_subject')->nullable();
+            $table->foreign('id_subject')->references('id')->on('subjects');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
---
+++ database/migrations/2021_12_03_191911_create_group_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_students', function (Blueprint $table) {
+            $table->id();
+
+            $table->boolean('is_leader');
+
+            $table->unsignedBigInteger('id_group')->nullable();
+            $table->foreign('id_group')->references('id')->on('groups');
+      
+            $table->unsignedBigInteger('id_student')->nullable();
+            $table->foreign('id_student')->references('id')->on('students');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_students');
+    }
+}
